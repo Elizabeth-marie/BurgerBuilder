9ef2e4e9d9290e2ed59172311d9b353e17034c49
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2019-05-24 09:16:17 -0600

    price displaying and updating based on add/remove button clicks

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,7 @@ class BurgerBuilder extends Component {
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
+          price={this.state.totalPrice}
           ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledButton} />
       </Aux>
