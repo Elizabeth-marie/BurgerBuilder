61f4f1bf03bf373e8189f8993b300b5c69c97667
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2019-05-24 07:44:27 -0600

    instructions added for users to begin selecting ingredients and css on burger tweaked so the seeds do not go off top bun

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -5,12 +5,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
 
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       })
     })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+
+  if (!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
   return (
     <div className={classes.BurgerContainer}>
       <div className={classes.Burger}>
